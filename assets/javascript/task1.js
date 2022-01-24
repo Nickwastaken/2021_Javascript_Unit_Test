fbc0f7d449549e2ded21538821f9f4bfefd00457
11172 <nicholas.mccann@vtsd.com>
2022-01-24 10:06:28 -0500

    Javascript Test

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,10 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
-	
+function buttonTask1() {
+    var buttonTask1 = document.getElementById(buttonTask1);
+    buttonTask1.style.display = "block";
+}
+
+function imageTask1() {
+    var imageTask1 = document.getElementById(imageTask1);
+    imageTask1.style.display = "none";
 }
