d1074f34778b37ad921da95feae2039f81f4dcb4
11172 <nicholas.mccann@vtsd.com>
2022-01-25 09:23:48 -0500

    Unit Test

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,9 @@
 /* Put code for the task below */
+function myFunction(e) {
+    //change the background color of the row
+}
+function myTable(){
+    var tbl = document.getElementById("tbl");
+    tbl.style.backgroundColor = "red"
+
+}
